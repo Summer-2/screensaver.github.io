8be93263e46e3a0f3cd049f57d6a5ec61032a135
Summer-2 <66897427+Summer-2@users.noreply.github.com>
2020-06-15 22:23:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-
 var today = new Date();
 var hourNow = today.getHours();
 
 var videoSources = [
-    "video/video10.mp4", // 00:00
-    "video/video10.mp4", // 01:00
-    "video/video10.mp4", // 02:00
-    "video/video10.mp4", // 03:00
-    "video/video10.mp4", // 04:00
-    "video/video11.mp4", // 05:00
-    "video/video11.mp4", // 06:00
-    "video/video12.mp4", // 07:00
-    "video/video13.mp4", // 08:00
-    "video/video14.mp4", // 09:00
-    "video/video15.mp4", // 10:00
-    "video/video1.mp4", // 11:00
-    "video/video2.mp4", // 12:00
-    "video/video3.mp4", // 13:00
-    "https://www.youtube.com/embed/7BQ_65HptQM?autoplay=1&loop=1", // 14:00
-    "https://www.youtube.com/embed/iJ1ImABsvFQ?autoplay=1&loop=1", // 15:00
-    "https://www.youtube.com/embed/iJ1ImABsvFQ?autoplay=1&loop=1", // 16:00
-    "https://www.youtube.com/embed/fGTm3e3_KrA?autoplay=1&loop=1", // 17:00
-    "https://www.youtube.com/embed/fGTm3e3_KrA?autoplay=1&loop=1", // 18:00
-    "https://www.youtube.com/embed/FmbKZIcMYTg?autoplay=1&loop=1", // 19:00
-    "https://www.youtube.com/embed/q8zkKWaQDD0?autoplay=1&loop=1", // 20:00
-    "https://www.youtube.com/embed/q8zkKWaQDD0?autoplay=1&loop=1", // 21:00
-    "https://www.youtube.com/embed/Rh8x40Vu7JA?autoplay=1&loop=1", // 22:00
-    "https://www.youtube.com/embed/Rh8x40Vu7JA?autoplay=1&loop=1" // 23:00
+    "https://www.youtube.com/embed/xylKLiSh8Ck?autoplay=1&controls=0&loop=1&showinfo=0", // 00:00
+    "https://www.youtube.com/embed/xylKLiSh8Ck?autoplay=1&controls=0&loop=1&showinfo=0", // 01:00
+    "https://www.youtube.com/embed/xylKLiSh8Ck?autoplay=1&controls=0&loop=1&showinfo=0", // 02:00
+    "https://www.youtube.com/embed/xylKLiSh8Ck?autoplay=1&controls=0&loop=1&showinfo=0", // 03:00
+    "https://www.youtube.com/embed/xylKLiSh8Ck?autoplay=1&controls=0&loop=1&showinfo=0", // 04:00
+    "https://www.youtube.com/embed/o-KTeGD8bQk?autoplay=1&controls=0&loop=1&showinfo=0", // 05:00
+    "https://www.youtube.com/embed/2G_9uk4tdpo?autoplay=1&controls=0&loop=1&showinfo=0", // 06:00
+    "https://www.youtube.com/embed/WecowY0AkNU?autoplay=1&controls=0&loop=1&showinfo=0", // 07:00
+    "https://www.youtube.com/embed/w1j-gieHlc8?autoplay=1&controls=0&loop=1&showinfo=0", // 08:00
+    "https://www.youtube.com/embed/1vmgGBMYYOo?autoplay=1&controls=0&loop=1&showinfo=0", // 09:00
+    "https://www.youtube.com/embed/XlaCcQ_TDZQ?autoplay=1&controls=0&loop=1&showinfo=0", // 10:00
+    "https://www.youtube.com/embed/wYH7ev6sNH0?autoplay=1&controls=0&loop=1&showinfo=0", // 11:00
+    "https://www.youtube.com/embed/9eApBKJq-D0?autoplay=1&controls=0&loop=1&showinfo=0", // 12:00
+    "https://www.youtube.com/embed/BAkWU3al41A?autoplay=1&controls=0&loop=1&showinfo=0", // 13:00
+    "https://www.youtube.com/embed/7BQ_65HptQM?autoplay=1&controls=0&loop=1&showinfo=0", // 14:00
+    "https://www.youtube.com/embed/iJ1ImABsvFQ?autoplay=1&controls=0&loop=1&showinfo=0", // 15:00
+    "https://www.youtube.com/embed/iJ1ImABsvFQ?autoplay=1&controls=0&loop=1&showinfo=0", // 16:00
+    "https://www.youtube.com/embed/fGTm3e3_KrA?autoplay=1&controls=0&loop=1&showinfo=0", // 17:00
+    "https://www.youtube.com/embed/fGTm3e3_KrA?autoplay=1&controls=0&loop=1&showinfo=0", // 18:00
+    "https://www.youtube.com/embed/FmbKZIcMYTg?autoplay=1&controls=0&loop=1&showinfo=0", // 19:00
+    "https://www.youtube.com/embed/q8zkKWaQDD0?autoplay=1&controls=0&loop=1&showinfo=0", // 20:00
+    "https://www.youtube.com/embed/q8zkKWaQDD0?autoplay=1&controls=0&loop=1&showinfo=0", // 21:00
+    "https://www.youtube.com/embed/Rh8x40Vu7JA?autoplay=1&controls=0&loop=1&showinfo=0", // 22:00
+    "https://www.youtube.com/embed/Rh8x40Vu7JA?autoplay=1&controls=0&loop=1&showinfo=0" // 23:00
   ];
 
 
